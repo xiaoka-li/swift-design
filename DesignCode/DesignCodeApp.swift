655da5e8d31c6882138ea38ca96be847b66deb12
xiaoka <ahuibin@163.com>
2022-10-22 18:14:34 +0800

    Initial Commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by xiaoka on 2022/10/22.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
