8e95a31a61829b14c2cfec76cc62e6bdf5b72589
xiaoka <ahuibin@163.com>
2022-10-22 22:37:12 +0800

    init project

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -9,13 +9,54 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack (alignment: .leading, spacing: 8.0){
+            Spacer()
+            Image("Logo 2")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 26.0, height: 26.0)
+                .cornerRadius(20.0)
+                .padding(9)
+                .background(.ultraThinMaterial,in:RoundedRectangle(cornerRadius: 30,style: .continuous))
+            Text("Hello, xiaoka!")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundStyle(.linearGradient(colors: [.primary,.primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
+                .lineLimit(1)
+            Text("this is text".uppercased())
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .foregroundStyle(.secondary)
+            Text("Access 120+ hours of courses, tutorials and livestreams")
+                .font(.footnote)
+                .multilineTextAlignment(.leading)
+                .lineLimit(2)
+                .foregroundColor(.secondary)
+//                .offset(y:50)
         }
-        .padding()
+        .padding(.all, 20.0)
+        .padding(.vertical,20.0)
+        .frame(height: 350.0)
+        .background(.ultraThinMaterial,in:RoundedRectangle(cornerRadius: 30,style: .continuous))
+//        .cornerRadius(30.0)
+//        .mask(RoundedRectangle(cornerRadius: 30,style: .continuous))
+        .shadow(color: Color("Shadow").opacity(0.3), radius: 10,x: 0,y: 10)
+        .overlay(
+            RoundedRectangle(cornerRadius: 30,style: .continuous)
+                .stroke(.linearGradient(colors: [.white.opacity(0.3),.black.opacity(0.1)], startPoint: .top, endPoint: .bottom))
+            .blendMode(.overlay)
+        )
+        .padding(.horizontal,20.0)
+        .background(
+            Image("Blob 1").offset(x:250,y: -100)
+        )
+        .overlay(
+            Image("Illustration 5")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 230)
+                .offset(x:50,y: -80)
+        )
     }
 }
 
